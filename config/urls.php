0cff9542cf01bb102ab3b9b68384895d1c7fa7e9
Iaol12 <frantisekboldis@gmail.com>
2025-12-09 09:09:40 +0000

    removed nested api

--- config/urls.php
+++ config/urls.php
@@ -3,5 +3,5 @@ return [
 	['class' => 'yii\rest\UrlRule', 'controller' => ['event' => 'api/event'], 'prefix' => 'api', 'pluralize' => false],
     ['class' => 'yii\rest\UrlRule', 'controller' => ['event-type' => 'api/event-type'], 'prefix' => 'api', 'pluralize' => false],
     ['class' => 'yii\rest\UrlRule', 'controller' => ['dashboard' => 'api/dashboard'], 'prefix' => 'api', 'pluralize' => false],
-    ['class' => 'yii\rest\UrlRule', 'controller' => ['dashboard/dashboard-widget' => 'api/dashboard/dashboard-widget'], 'prefix' => 'api', 'pluralize' => false],
+    ['class' => 'yii\rest\UrlRule', 'controller' => ['dashboard-widget' => 'api/dashboard-widget'], 'prefix' => 'api', 'pluralize' => false],
 ];
--- controllers/api/DashboardWidgetController.php
+++ controllers/api/DashboardWidgetController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\controllers\api\dashboard;
+namespace app\controllers\api;
 
 use Yii;
 use app\models\Dashboard;
