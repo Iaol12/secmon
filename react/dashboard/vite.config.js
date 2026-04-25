099a0e27cfebf7d9a5520f269b6d00813cb1e067
iaol12 <frantisekboldis@gmail.com>
2026-04-25 22:29:24 +0300

    removed proxy in vite config

--- react/dashboard/vite.config.js
+++ react/dashboard/vite.config.js
@@ -8,40 +8,6 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    // open: true,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:8443',
-        changeOrigin: true,
-        secure: false,
-        rejectUnauthorized: false,
-        configure: (proxy, options) => {
-          proxy.on('error', (err, _req, res) => {
-            console.log('proxy error', err);
-            res.writeHead(500, {
-              'Content-Type': 'text/plain',
-            });
-            res.end('Proxy error: ' + err.message);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            proxyReq.setHeader('Authorization', 'Bearer dpn--jlmyVmqyoW0nXYjbjrEtxa_C5KU');
-
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-      '/data': {
-        target: 'https://localhost:8443',
-        changeOrigin: true,
-        secure: false,
-      }
-    },
-  },
   build: {
       outDir: path.resolve(__dirname, '../../web/js/dist'),
       emptyOutDir: true,
